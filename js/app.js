abb7e140d47ade4380dab95b27f9e2b04b9f3cb4
geng0610 <geng0610@gmail.com>
2015-05-15 02:54:46 -0400

    Removing PlacesService
    PlacesService limits request to 10 request per time and returns results
    in a batch.

--- js/app.js
+++ js/app.js
@@ -4,32 +4,41 @@ var locations = [];
 
 //Create a model we will be using based on raw model
 
-for (var item in swarmExport.response.checkins.items) {
-    var item_id = swarmExport.response.checkins.items[item].venue.id;
-    if (checkNewLocations(locations, item_id)){
-        var single_location = {};
-        single_location.id=item_id;
-        single_location.venueName=swarmExport.response.checkins.items[item].venue.name;
-        single_location.venueFormattedAddress=swarmExport.response.checkins.items[item].venue.location.address+", "+swarmExport.response.checkins.items[item].venue.location.city+", "+swarmExport.response.checkins.items[item].venue.location.state+" "+swarmExport.response.checkins.items[item].venue.location.postalCode;
-        single_location.venueLat=swarmExport.response.checkins.items[item].venue.location.lat;;
-        single_location.venueLng=swarmExport.response.checkins.items[item].venue.location.lng;;
-        locations.push(single_location);
-    }
-};
-
-//Make sure duplicate locations are not included.
 
 function checkNewLocations(current_locations, current_item_id){
     var new_item = true;
     for (var i in current_locations) {
-        if(current_locations[i].item_id == current_item_id){
-            console.log(current_locations[i].item_id+ "duplicate name" + current_item_id);
+        //console.log(current_locations, current_item_id)
+        if(current_locations[i].id == current_item_id){
+            //console.log(current_locations[i].item_id+ "duplicate name" + current_item_id);
             new_item = false;
             break;
         }
     }
     return new_item;
 }
+
+for (var item in swarmExport.response.checkins.items) {
+    var item_id = swarmExport.response.checkins.items[item].venue.id;
+    //console.log("this is item id "+item_id)
+    if (checkNewLocations(locations, item_id)){
+        //console.log("this is a unique item id "+item_id)
+        var single_location = {};
+        single_location.id=item_id;
+        single_location.venueName=swarmExport.response.checkins.items[item].venue.name;
+        single_location.venueUrl=swarmExport.response.checkins.items[item].venue.url;
+        single_location.venueFormattedAddress=swarmExport.response.checkins.items[item].venue.location.address+", "+swarmExport.response.checkins.items[item].venue.location.city+", "+swarmExport.response.checkins.items[item].venue.location.state+" "+swarmExport.response.checkins.items[item].venue.location.postalCode;
+        single_location.venueLat=swarmExport.response.checkins.items[item].venue.location.lat;
+        single_location.venueLng=swarmExport.response.checkins.items[item].venue.location.lng;
+        //console.log("this is a single location", single_location);
+        locations.push(single_location);
+        //console.log("locations");
+        //console.log(locations[locations.length-1]);
+    }
+};
+//console.log("this is a list of locations location", locations)
+//Make sure duplicate locations are not included.
+
 
 var midpoint = {};
 var lat_min = locations[0].venueLat;
@@ -70,6 +79,14 @@ var zoomLevel = calculateZoom();
 
 //console.log(locations);
 
+function sleep(miliseconds) {
+           var currentTime = new Date().getTime();
+
+           while (currentTime + miliseconds >= new Date().getTime()) {
+           }
+       }
+
+
 var mapView = {
     init: function(locations, midpoint){
         var center=new google.maps.LatLng(midpoint.lat,midpoint.lng);
@@ -78,57 +95,36 @@ var mapView = {
           zoom: zoomLevel
         };
         var map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
-        //console.log(locations);
+        function callback(results, status) {
+            sleep(2000);
+            if (status == google.maps.places.PlacesServiceStatus.OK) {
+                var place = results[0];
+                var marker = new google.maps.Marker({
+                    position: place.geometry.location,
+                    map: map 
+                });
+            } else{console.log(status)}
+        }
+        sleep(500);
         for (var i in locations){
             var myLat = locations[i].venueLat;
             var myLng = locations[i].venueLng;
             var myLatLng = new google.maps.LatLng(myLat,myLng);
             var myFormattedAddress = locations[i].venueFormattedAddress;
-            var request = {
+            /*var request = {
                 location: myLatLng,
                 radius: '500',
                 query: myFormattedAddress
             };
             var service = new google.maps.places.PlacesService(map);
             service.textSearch(request, callback);
-            function callback(results, status) {
-              if (status == google.maps.places.PlacesServiceStatus.OK) {
-                //for (var i = 0; i < results.length; i++) {
-                    var place = results[0];
-                    console.log(place);
-                    var marker = new google.maps.Marker({
-                        position: new google.maps.LatLng(40.7127,-74.0059),
-                        map: map 
-                    });
-                //}
-              }
-            }
-
-            //console.log(myLatLng);
-            /*var marker = new google.maps.Marker({
+            console.log("requested "+ myFormattedAddress);*/
+            var marker = new google.maps.Marker({
                 position: myLatLng,
-                map: map
-            });*/
+                map:map
+            });
+            sleep(100);
         }
-
-        /*var request = {
-            location: new google.maps.LatLng(40.7127,-74.0059),
-            radius: '500',
-            query: "Eugene O'Neill Theatre 230 W 49th St (btwn Broadway & 8th Ave) New York, NY 10019"
-        };
-        service = new google.maps.places.PlacesService(map);
-        service.textSearch(request, callback)
-        function callback(results, status) {
-              if (status == google.maps.places.PlacesServiceStatus.OK) {
-                for (var i = 0; i < results.length; i++) {
-                    var place = results[i];
-                    var marker = new google.maps.Marker({
-                        position: new google.maps.LatLng(40.7127,-74.0059),
-                        map: map 
-                    });
-                }
-              }
-            }*/
         google.maps.event.addDomListener(window, "resize", function() {
             var newCenter = map.getCenter();
             google.maps.event.trigger(map, "resize");
